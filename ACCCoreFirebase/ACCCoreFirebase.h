65f8faeed491cae3ad6732dd870f223e6f8784c5
HoanNL <hoannguyen@vulcanlabs.co>
2024-04-17 18:10:20 +0700

    firebase core protocols

---
+++ ACCCoreFirebase/ACCCoreFirebase.h
@@ -0,0 +1,18 @@
+//
+//  ACCCoreFirebase.h
+//  ACCCoreFirebase
+//
+//  Created by HoanNL on 17/04/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ACCCoreFirebase.
+FOUNDATION_EXPORT double ACCCoreFirebaseVersionNumber;
+
+//! Project version string for ACCCoreFirebase.
+FOUNDATION_EXPORT const unsigned char ACCCoreFirebaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACCCoreFirebase/PublicHeader.h>
+
+
---
+++ setup_carthage.sh
@@ -0,0 +1,2 @@
+carthage update --no-build
+carthage build --platform iOS --cache-builds --no-use-binaries --use-xcframeworks --cache-builds
