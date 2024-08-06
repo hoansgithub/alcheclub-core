ff456a3dd9014b7752a72b86622a87b58a0f60ab
HoanNL <hoannguyen@vulcanlabs.co>
2024-08-06 14:28:11 +0700

    public StoreViewProductPaymentMode

--- ACCCoreStoreKit/StoreView/StoreViewProduct.swift
+++ ACCCoreStoreKit/StoreView/StoreViewProduct.swift
@@ -7,19 +7,33 @@
 
 import Foundation
 import StoreKit
+
+public enum StoreViewProductPaymentMode: Int, Sendable, Codable {
+
+    /// Price is charged one or more times
+    case payAsYouGo = 0
+    /// Price is charged once in advance
+    case payUpFront = 1
+    /// No initial charge
+    case freeTrial = 2
+
+}
+
 public struct StoreViewProduct: Sendable, Codable, Identifiable {
     public let id: String
     public var labels: [String: String]
     public var purchased: Bool = false
     public let displayPrice: String
     public let displayName: String
+    public let paymentMode: StoreViewProductPaymentMode?
     
-    public init(id: String, labels: [String : String], purchased: Bool = false, displayPrice: String, displayName: String) {
+    public init(id: String, labels: [String : String], purchased: Bool = false, displayPrice: String, displayName: String, paymentMode: StoreViewProductPaymentMode? = nil) {
         self.id = id
         self.labels = labels
         self.purchased = purchased
         self.displayPrice = displayPrice
         self.displayName = displayName
+        self.paymentMode = paymentMode
     }
 }
 
